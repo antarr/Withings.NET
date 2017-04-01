e6727736c195c16bef335058ce78cb80884fca86
Antarr Byrd <antarr.byrd@gmail.com>
2017-04-01 12:35:29 -0500

    User RestSharp for GetActivityMeasures

--- Withings.NET/Client/WithingsClient.cs
+++ Withings.NET/Client/WithingsClient.cs
@@ -1,22 +1,22 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using System.Text;
-using Material.Infrastructure.Credentials;
+﻿using Material.Infrastructure.Credentials;
 using RestSharp;
 using RestSharp.Authenticators;
 using RestSharp.Authenticators.OAuth;
 using RestSharp.Extensions.MonoHttp;
+using System;
+using System.Text;
 
 namespace Withings.NET.Client
 {
     public class WithingsClient
     {
         internal RestClient Client;
-        private readonly OAuth1Credentials _credentials;
+        readonly OAuth1Credentials _credentials;
+        string baseUri = "http://wbsapi.withings.net/v2";
+
         public WithingsClient(OAuth1Credentials credentials)
         {
-            Client = new RestClient("http://wbsapi.withings.net/")
+            Client = new RestClient(baseUri)
             {
                 Authenticator = OAuth1Authenticator.ForProtectedResource
                 (
@@ -32,35 +32,61 @@ namespace Withings.NET.Client
 
         public string GetActivityMeasures(string startDay, string endDay, string userId, string token, string secret)
         {
+            var request = new RestRequest("measures");
+            request.AddParameter("action", "getactivity");
+            request.AddParameter("userid", userId);
+            request.AddParameter("startdateymd", startDay);
+            request.AddParameter("enddateymd", endDay);
             var oAuth = new OAuthBase();
 
             var nonce = Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
             var timeStamp = oAuth.GenerateTimeStamp();
-            var uri = $"http://wbsapi.withings.net/v2/measure?action=getactivity&userid={userId}&startdateymd={startDay}&enddateymd={endDay}";
+            var uri = $"{baseUri}measure?action=getactivity&userid={userId}&startdateymd={startDay}&enddateymd={endDay}";
             string normalizedUrl;
             string parameters;
-            var signature = oAuth.GenerateSignature(new Uri(uri), _credentials.ConsumerKey, _credentials.ConsumerSecret,
-                token, secret, "GET", timeStamp, nonce,
-                OAuthBase.SignatureTypes.HMACSHA1, out normalizedUrl, out parameters);
+            var signature = oAuth.GenerateSignature
+                (
+                    new Uri(uri),
+                    _credentials.ConsumerKey,
+                  _credentials.ConsumerSecret,
+                   token,
+                   secret,
+                   "GET",
+                   timeStamp,
+                   nonce,
+                    OAuthBase.SignatureTypes.HMACSHA1,
+                    out normalizedUrl,
+                    out parameters
+                );
 
             signature = HttpUtility.UrlEncode(signature);
 
-            var requestUri = new StringBuilder(uri);
-            requestUri.AppendFormat("&oauth_consumer_key={0}&", _credentials.ConsumerKey);
-            requestUri.AppendFormat("oauth_nonce={0}&", nonce);
-            requestUri.AppendFormat("oauth_signature={0}&", signature);
-            requestUri.AppendFormat("oauth_signature_method={0}&", "HMAC-SHA1");
-            requestUri.AppendFormat("oauth_timestamp={0}&", timeStamp);
-            requestUri.AppendFormat("oauth_token={0}&", token);
-            requestUri.AppendFormat("oauth_version={0}", "1.0");
+            request.AddParameter("oauth_consumer_key", _credentials.ConsumerKey);
+            request.AddParameter("oauth_nonce", nonce);
+            request.AddParameter("oauth_signature", signature);
+            request.AddParameter("oauth_signature_method", "HMAC-SHA1");
+            request.AddParameter("oauth_timestamp", timeStamp);
+            request.AddParameter("oauth_token", token);
+            request.AddParameter("oauth_version", "1.0");
 
-            var wrGeturl = WebRequest.Create(requestUri.ToString());
-            var objStream = wrGeturl.GetResponse().GetResponseStream();
-            const string sLine = "";
+            var response = Client.Execute(request);
+            return response.Content;
+            //var requestUri = new StringBuilder(uri);
+            //requestUri.AppendFormat("&oauth_consumer_key={0}&", _credentials.ConsumerKey);
+            //requestUri.AppendFormat("oauth_nonce={0}&", nonce);
+            //requestUri.AppendFormat("oauth_signature={0}&", signature);
+            //requestUri.AppendFormat("oauth_signature_method={0}&", "HMAC-SHA1");
+            //requestUri.AppendFormat("oauth_timestamp={0}&", timeStamp);
+            //requestUri.AppendFormat("oauth_token={0}&", token);
+            //requestUri.AppendFormat("oauth_version={0}", "1.0");
 
-            if (objStream == null) return sLine;
-            var objReader = new StreamReader(objStream);
-            return objReader.ReadLine();
+            //var wrGeturl = WebRequest.Create(requestUri.ToString());
+            //var objStream = wrGeturl.GetResponse().GetResponseStream();
+            //const string sLine = "";
+
+            //if (objStream == null) return sLine;
+            //var objReader = new StreamReader(objStream);
+            //return objReader.ReadLine();
         }
     }
 }
